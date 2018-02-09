b308a5595df9fb796996168a28d524e8355320bf
ogbuachi <ogbuachi@kth.se>
2018-02-09 14:05:38 +0000

    uncommented the print that crashed

---
+++ app/lab_1/src_0/cpu_0.c
@@ -0,0 +1,418 @@
+// Skeleton for lab 2
+// 
+// Task 1 writes periodically RGB-images to the shared memory
+//
+// No guarantees provided - if bugs are detected, report them in the Issue tracker of the github repository!
+
+#include <stdio.h>
+#include "altera_avalon_performance_counter.h"
+#include "includes.h"
+#include "altera_avalon_pio_regs.h"
+#include "sys/alt_irq.h"
+#include "sys/alt_alarm.h"
+#include "system.h"
+#include "io.h"
+#include <math.h>
+#include "images.h"
+
+#define DEBUG 1
+
+#define HW_TIMER_PERIOD 100 /* 100ms */
+
+/* Definition of Task Stacks */
+#define   TASK_STACKSIZE       2048
+OS_STK    task1_stk[TASK_STACKSIZE];
+OS_STK    StartTask_Stack[TASK_STACKSIZE]; 
+
+/* Definition of Task Priorities */
+
+#define STARTTASK_PRIO      1
+#define TASK1_PRIORITY      10
+
+/* Definition of Task Periods (ms) */
+#define TASK1_PERIOD 10000
+
+#define SECTION_1 1
+
+/* Variables */
+INT8U ENABLE = 0;
+INT8U hmin = 0, hmax = 0;
+
+/*
+ * Example function for copying a p3 image from sram to the shared on-chip mempry
+ */
+void sram2sm_p3(unsigned char* base)
+{
+	int x, y;
+	unsigned char* shared;
+
+	shared = (unsigned char*) SHARED_ONCHIP_BASE;
+
+	int size_x = *base++;
+	int size_y = *base++;
+	int max_col= *base++;
+	*shared++  = size_x;
+	*shared++  = size_y;
+	*shared++  = max_col;
+	printf("The image is: %d x %d!! \n", size_x, size_y);
+	for(y = 0; y < size_y; y++)
+	for(x = 0; x < size_x; x++)
+	{
+		*shared++ = *base++; 	// R
+		*shared++ = *base++;	// G
+		*shared++ = *base++;	// B
+	}
+}
+
+/*
+ * Global variables
+ */
+int delay; // Delay of HW-timer 
+
+/*
+ * ISR for HW Timer
+ */
+alt_u32 alarm_handler(void* context)
+{
+  OSTmrSignal(); /* Signals a 'tick' to the SW timers */
+  
+  return delay;
+}
+
+// Semaphores
+OS_EVENT *Task1TmrSem;
+
+// SW-Timer
+OS_TMR *Task1Tmr;
+
+/* Timer Callback Functions */ 
+void Task1TmrCallback (void *ptmr, void *callback_arg){
+  OSSemPost(Task1TmrSem);
+}
+
+/* Image Processing Functions */
+
+void control(int a){
+	if(a < 128)
+		ENABLE = 1;
+	else
+		ENABLE = 0;
+}
+
+void grayscale(int row, int col, INT8U *image, INT8U *grayImage){
+	//float aux[row][col];
+	int i = 0, j = 0, x = row*col*3;	
+	
+	while(i < x){
+		grayImage[j] = image[i]*0.3125 + image[i+1]*0.5625 + image[i+2]*0.125; //change these floating point operations into bitwise
+		i = i+3;
+		j++;
+	}
+}
+
+void resize(int x1, int y1, INT8U* grayImage, INT8U* resizedImage){
+	if((x1%2) == 0 && (y1%2) == 0){ //col = y1
+		int i = 0, j = 0, f = 1, colSum = 2, size1 = x1*y1, col = y1; //i is index for grayImage, j is index for resizedImage
+		INT8U resizedPixel = 0;
+		int newLine = 0, size2 = (x1/2)*(y1/2);
+		while(j < size2){
+			resizedPixel = (grayImage[i] + grayImage[i+1] + grayImage[i+col] + grayImage[i+1+col])/4;
+			resizedImage[j] = resizedPixel;
+			if(f%(col/2) == 0){
+				i = col * colSum;
+				colSum = colSum + 2;
+				newLine = 1;
+			}
+			else{
+				i = i + 2;
+				newLine = 0;
+			}
+			f++;
+			j++;
+		}
+	}
+}
+
+void sobel(int x2, int y2, INT8U* image, INT8U* edgeImage){
+	int kx[9] = {-1,0,1,-2,0,2,-1,0,1};
+	int ky[9] = {1,2,1,0,0,0,-1,-2,-1};
+	int i = 0, j = 0, z = 1, f = 1, /*size = x2*y2,*/ limit = (x2-2)*(y2-2)/*size - 2*y2*/, col = y2;
+	INT8U gx = 0, gy = 0, g = 0;
+	int newLine = 1;
+	while(j < limit){
+		gx = image[i]*kx[0] + image[i+1]*kx[1] + image[i+2]*kx[2]
+		   + image[i+col]*kx[3] + image[i+col+1]*kx[4] + image[i+col+2]*kx[5]
+		   + image[i+(2*col)]*kx[6] + image[i+(2*col)+1]*kx[7] + image[i+(2*col)+2]*kx[8];
+		
+		gy = image[i]*ky[0] + image[i+1]*ky[1] + image[i+2]*ky[2]
+		   + image[i+col]*ky[3] + image[i+col+1]*ky[4] + image[i+col+2]*ky[5]
+		   + image[i+(2*col)]*ky[6] + image[i+(2*col)+1]*ky[7] + image[i+(2*col)+2]*ky[8];
+		
+		//g = sqrt(gx*gx + gy*gy); //change with square root algorithm
+		g = gx + gy;
+		edgeImage[j] = g;
+		
+		if(f/(col-2) == z && i > 0 && !newLine){
+			i = /*i + 3*/col*z;
+			newLine = 1;
+			z++; //current line in a 2D type of view
+		}
+		else{
+			newLine = 0;
+			i++;
+		}
+		
+		f++;
+		j++;
+	}
+}
+
+void toAsciiArt(int row, int col, INT8U *image, char *asciiImage){
+	char asciiLevels[16] = {' ','.',':','-','=','+','/','t','z','U','w','*','0','#','%','@'};
+	
+	int i = 0, size = row*col;
+	
+	while(i < size){
+		asciiImage[i] = asciiLevels[((int)image[i])%16];
+		i++;
+	}
+}
+
+INT8U brightness(int size, INT8U *min, INT8U *max, INT8U* array){
+	*min = 10000;
+	*max = -10000;
+	int i = 0;
+	INT8U avg = 0; //holds the sums for the average brightness
+	while(i < size){
+		if(array[i] < *min)
+			*min = array[i];
+		if(array[i] > *max)
+			*max = array[i];
+		avg = avg + array[i];
+		i++;
+	}
+	avg = avg/(i+1); //actual brightness
+	return avg;
+}
+
+void correction(int size, INT8U *array){
+	control(brightness(size, &hmin, &hmax, array));
+	if(ENABLE){
+		INT8U sub = hmax - hmin;
+		int i = 0, mul = 1;
+		if(sub <= 127){
+			if(sub > 63)
+				mul = 2;
+			else{
+				if(sub > 31)
+					mul = 4;
+				else{
+					if(sub > 15)
+						mul = 8;
+					else
+						mul = 16;
+				}
+			}
+			while(i < size){
+				array[i] = (array[i] - hmin) * mul;
+				i++;
+			}
+		}
+	}
+}
+
+void task1(void* pdata)
+{
+	INT8U err;
+	INT8U value=0;
+	char current_image=0;
+	
+	#if DEBUG == 1
+	/* Sequence of images for testing if the system functions properly */
+	char number_of_images=10;
+	unsigned char* img_array[10] = {img1_24_24, img1_32_22, img1_32_32, img1_40_28, img1_40_40, 
+			img2_24_24, img2_32_22, img2_32_32, img2_40_28, img2_40_40 };
+	#else
+	/* Sequence of images for measuring performance */
+	char number_of_images=3;
+	unsigned char* img_array[3] = {img1_32_32, img2_32_32, img3_32_32};
+	#endif
+
+	while (1)
+	{ 
+		/* Extract the x and y dimensions of the picture */
+		unsigned char i = *img_array[current_image];
+		unsigned char j = *(img_array[current_image]+1);
+
+		PERF_RESET(PERFORMANCE_COUNTER_0_BASE);
+		PERF_START_MEASURING (PERFORMANCE_COUNTER_0_BASE);
+		PERF_BEGIN(PERFORMANCE_COUNTER_0_BASE, SECTION_1);
+		
+		/* Measurement here */
+		sram2sm_p3(img_array[current_image]);
+		
+		/* Image Processing Execution */
+		int /*i = 0,*/ /*size1 = (x*3)*y,*/ size2 = i*j, size3 = i*j/4, size4 = (i/2-2)*(j/2-2);
+		
+		INT8U grayImage[size2];
+		INT8U resizedImage[size3];
+		INT8U edgeImage[size4];
+		char asciiImage[size4];
+		
+		grayscale(i, j, img_array[current_image]+3, grayImage);
+		resize(i, j, grayImage, resizedImage);
+		correction(size3, resizedImage);
+		sobel(i/2,j/2, resizedImage, edgeImage);
+		toAsciiArt((i/2)-2, (j/2)-2, edgeImage, asciiImage);
+		
+		PERF_END(PERFORMANCE_COUNTER_0_BASE, SECTION_1);  
+		
+		if(DEBUG){
+			printf("---- Gray Image ----");
+			int z = 0;
+			
+			while(z < size2){
+			//grayscale (put "i < size2" as condition for the while loop)
+			printf((int)grayImage[z]+" ");
+			if((z+1)%j == 0 && z > 0)
+				printf("\n");
+			z++;
+			}
+		}
+
+		/* Print report */
+		perf_print_formatted_report
+		(PERFORMANCE_COUNTER_0_BASE,            
+		ALT_CPU_FREQ,        // defined in "system.h"
+		1,                   // How many sections to print
+		"Section 1"        // Display-name of section(s).
+		);   
+
+		/* Just to see that the task compiles correctly */
+		IOWR_ALTERA_AVALON_PIO_DATA(LEDS_GREEN_BASE,value++);
+
+		OSSemPend(Task1TmrSem, 0, &err);
+
+		/* Increment the image pointer */
+		current_image=(current_image+1) % number_of_images;
+
+	}
+}
+
+void StartTask(void* pdata)
+{
+  INT8U err;
+  void* context;
+
+  static alt_alarm alarm;     /* Is needed for timer ISR function */
+  
+  /* Base resolution for SW timer : HW_TIMER_PERIOD ms */
+  delay = alt_ticks_per_second() * HW_TIMER_PERIOD / 1000; 
+  printf("delay in ticks %d\n", delay);
+
+  /* 
+   * Create Hardware Timer with a period of 'delay' 
+   */
+  if (alt_alarm_start (&alarm,
+      delay,
+      alarm_handler,
+      context) < 0)
+      {
+          printf("No system clock available!n");
+      }
+
+  /* 
+   * Create and start Software Timer 
+   */
+
+   //Create Task1 Timer
+   Task1Tmr = OSTmrCreate(0, //delay
+                            TASK1_PERIOD/HW_TIMER_PERIOD, //period
+                            OS_TMR_OPT_PERIODIC,
+                            Task1TmrCallback, //OS_TMR_CALLBACK
+                            (void *)0,
+                            "Task1Tmr",
+                            &err);
+                            
+   if (DEBUG) {
+    if (err == OS_ERR_NONE) { //if creation successful
+      printf("Task1Tmr created\n");
+    }
+   }
+   
+
+   /*
+    * Start timers
+    */
+   
+   //start Task1 Timer
+   OSTmrStart(Task1Tmr, &err);
+   
+   if (DEBUG) {
+    if (err == OS_ERR_NONE) { //if start successful
+      printf("Task1Tmr started\n");
+    }
+   }
+
+
+   /*
+   * Creation of Kernel Objects
+   */
+
+  Task1TmrSem = OSSemCreate(0);   
+
+  /*
+   * Create statistics task
+   */
+
+  OSStatInit();
+
+  /* 
+   * Creating Tasks in the system 
+   */
+
+  err=OSTaskCreateExt(task1,
+                  NULL,
+                  (void *)&task1_stk[TASK_STACKSIZE-1],
+                  TASK1_PRIORITY,
+                  TASK1_PRIORITY,
+                  task1_stk,
+                  TASK_STACKSIZE,
+                  NULL,
+                  0);
+
+  if (DEBUG) {
+     if (err == OS_ERR_NONE) { //if start successful
+      printf("Task1 created\n");
+    }
+   }  
+
+  printf("All Tasks and Kernel Objects generated!\n");
+
+  /* Task deletes itself */
+
+  OSTaskDel(OS_PRIO_SELF);
+}
+
+
+int main(void) {
+
+  printf("MicroC/OS-II-Vesion: %1.2f\n", (double) OSVersion()/100.0);
+     
+  OSTaskCreateExt(
+	 StartTask, // Pointer to task code
+         NULL,      // Pointer to argument that is
+                    // passed to task
+         (void *)&StartTask_Stack[TASK_STACKSIZE-1], // Pointer to top
+						     // of task stack 
+         STARTTASK_PRIO,
+         STARTTASK_PRIO,
+         (void *)&StartTask_Stack[0],
+         TASK_STACKSIZE,
+         (void *) 0,  
+         OS_TASK_OPT_STK_CHK | OS_TASK_OPT_STK_CLR);
+         
+  OSStart();
+  
+  return 0;
+}
